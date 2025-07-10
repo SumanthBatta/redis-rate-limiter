882b87f98538ab8cd3d9a064138af30243e9cf25
SumanthBatta <battasumanth1090@gmail.com>
2025-07-10 22:17:34 +0530

    Initial commit

---
+++ src/main/java/com/example/redisratelimiter/RedisratelimiterApplication.java
@@ -0,0 +1,13 @@
+package com.example.redisratelimiter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RedisratelimiterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RedisratelimiterApplication.class, args);
+	}
+
+}
